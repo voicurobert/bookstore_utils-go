03d13afefa44736d44782d9caca255d22bfc83bb
robert <voicu.eduardrobert@gmail.com>
2021-06-10 08:14:01 +0300

    mods

---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,45 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestError struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+
+func NewBadRequestError(message string) *RestError {
+	return &RestError{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestError {
+	return &RestError{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string, err error) *RestError {
+	result := &RestError{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+	if err != nil {
+		result.Causes = append(result.Causes, err.Error())
+	}
+	return result
+}
